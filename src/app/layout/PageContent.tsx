956a7195075bdd4cc86827f26daba3e29e54a189
MarinaSirenko-git <sirenko.workspace@gmail.com>
2025-12-30 11:04:23 +0700

    feat: redesign UI components to strict monochrome style

--- src/app/layout/PageContent.tsx
+++ src/app/layout/PageContent.tsx
@@ -7,7 +7,7 @@ function PageContent(){
         <div className="flex flex-col min-h-screen">
             <Header/>
             <main className="flex-1 overflow-y-auto z-10 h-full">
-                <div className="h-full w-full p-4 mt-1 h-full">
+                <div className="h-full w-full p-4 mt-1 h-full bg-white dark:bg-black">
                     <Outlet />
                 </div>
             </main>
